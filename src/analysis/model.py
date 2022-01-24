024ba0133530413561ba93a0913c13df542595b9
Yu <jiangyu9272@outlook.com>
2022-01-24 17:01:11 +0800

    Add Quad constant limitations.

--- src/analysis/model.py
+++ src/analysis/model.py
@@ -1,5 +1,28 @@
 import json
 
+PAD_TOKEN = '<PAD>'
+CALL_TOKEN = '<Call>'
+ASSIGN_TOKEN = '<Assign>'
+RETURN_TOKEN = '<Return>'
+CONST_TOKEN = '$Const$'
+SELF_TOKEN = '$'
+
+"""
+Length limitations for call extractor.
+"""
+MAX_ASSIGN_LEFT_VALUE = 3
+MAX_PARA_CONSTANT_LEN = 5
+
+
+"""
+Length limitations for quad fields(for storage, 
+different with length using in data processing.).
+"""
+STORAGE_MAX_CTX_LEN = 7
+STORAGE_MAX_CALL_LEN = 7
+STORAGE_MAX_ACTOR_LEN = 3
+STORAGE_MAX_TYPE_LEN = 1
+STORAGE_MAX_PARAS_LEN = 3
 
 class Quad(object):
     def __init__(self):
